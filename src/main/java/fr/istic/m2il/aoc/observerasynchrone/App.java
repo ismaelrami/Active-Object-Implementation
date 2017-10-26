1b0fb908c91062608c61325b0effda94bfde9ce9
Soumaila Ramadan <ramadansum@gmail.com>
2017-10-26 19:49:50 +0200

    Modification de l'app pour test l'observer asynchrone

--- src/main/java/fr/istic/m2il/aoc/observerasynchrone/App.java
+++ src/main/java/fr/istic/m2il/aoc/observerasynchrone/App.java
@@ -1,5 +1,6 @@
 package fr.istic.m2il.aoc.observerasynchrone;
 
+import fr.istic.m2il.aoc.observerasynchrone.channel.Channel;
 import fr.istic.m2il.aoc.observerasynchrone.displayer.Displayer;
 import fr.istic.m2il.aoc.observerasynchrone.displayer.DisplayerImpl;
 import fr.istic.m2il.aoc.observerasynchrone.generator.Generator;
@@ -15,9 +16,11 @@ public class App
     {
         Generator generator = new GeneratorImpl();
         Displayer displayer1 = new DisplayerImpl();
-        generator.attach(displayer1);
+        Channel channel1 = new Channel(generator, displayer1);
+        generator.attach(channel1);
         Displayer displayer2 = new DisplayerImpl();
-        generator.attach(displayer2);
+        Channel channel2 = new Channel(generator, displayer2);
+        generator.attach(channel2);
         generator.generate();
     }
 }
